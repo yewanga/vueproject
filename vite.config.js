18391c75378ad1658e8fb0fcf738848a32a617fd
Ethanxchen <yxy18068328939@163.com>
2024-05-11 23:23:20 +0800

    init project

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server:{
+    host:'0.0.0.0',
+    port: '8080',
+    open:true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
+
